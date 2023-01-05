ee9f3de4315d199c5ba488bbf1f27656bc9c1ab1
EngineerLad <engineerlad5541@gmail.com>
2023-01-04 17:53:08 -0800

    Finished the first edition of the deMistify method. Will work on a method to exercise the instance model once trained.

--- test platform.py
+++ test platform.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 import scipy 
+import math
 
 from scipy.interpolate import interp1d
+print(10**-5)
+print (int(math.log10(0.73293)))
 
-
-x = np.arange(10,20)
+x = np.arange(10,20,.1)
 
 print('x:',x)
 
 
-y = np.exp(-x/10)
+y = [0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1]
 
 print('y:',y)
 
@@ -19,10 +21,10 @@ print('y:',y)
 f_cubic = scipy.interpolate.interp1d(x,y,kind = 'cubic') 
 
 
-xnew = np.arange(10,19,0.1)
+xnew = np.arange(10,20,.1)
 
 
-ynew = f_cubic(xnew) 
+ynew = f_cubic(x)
 
 print('new_x:',xnew)
 print('new_y:',ynew)
@@ -46,5 +48,7 @@ plt.title("1d Interpolation using scipy interp1d method")
 
 
 plt.show()
+
+
 
 
